05fb9b3bef31fcabdf9f2bf38dd1eb1bde81c49b
Maycon <jmaycon@users.noreply.github.com>
2026-02-11 01:44:18 +0100

    refactor(cdcapp): move AWS System properties to static block
    Moved System.setProperty calls for AWS configuration from main method to static initialization block for better initialization order and cleaner code

--- src/main/java/edu/jmaycon/cdcapp/CdcApplication.java
+++ src/main/java/edu/jmaycon/cdcapp/CdcApplication.java
@@ -6,10 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CdcApplication {
 
-    public static void main(String[] args) {
+    static {
         System.setProperty("aws.region", "eu-central-1");
         System.setProperty("aws.accessKeyId", "admin");
         System.setProperty("aws.secretAccessKey", "admin123");
+    }
+
+    public static void main(String[] args) {
         SpringApplication.run(CdcApplication.class, args);
     }
 }
